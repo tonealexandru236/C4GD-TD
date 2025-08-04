2353fc33f3a315e0bff52d4b5ba4e3afe2b5cbc9
Theman360 <158336022+tonealexandru236@users.noreply.github.com>
2025-08-04 14:53:20 -0400

    pause & double speed visuals

--- C4GD-TD/Assets/Scripts/MainButtons.cs
+++ C4GD-TD/Assets/Scripts/MainButtons.cs
@@ -1,27 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MainButtons : MonoBehaviour
 {
     private float speed = 1f;
+    public GameObject pause;
+    public TMP_Text speedup;
+
     private void Start()
     {
+        speedup.SetText("x1");
         Time.timeScale = speed;
     }
 
     public void game_pause()
     {
-        if(Time.timeScale == 0) 
+        if (Time.timeScale == 0)
+        {
             Time.timeScale = speed;
-        else
+            pause.SetActive(false);
+        }
+        else 
+        {
             Time.timeScale = 0;
-        
+            pause.SetActive(true);
+        }
+
     }
 
     public void game_speed()
     {
-        if (speed == 1) speed = 2;
-        else speed = 1;
+        if (speed == 1) { speed = 2; speedup.SetText("x2"); }
+        else { speed = 1; speedup.SetText("x1");
     }
+}
 }
