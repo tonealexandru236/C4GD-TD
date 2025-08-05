8e7cd8d1e86b4f69617003f87c6f38156d666163
Theman360 <158336022+tonealexandru236@users.noreply.github.com>
2025-08-05 12:00:12 -0400

    Shooting prototype

---
+++ C4GD-TD/Assets/Scripts/Shoot.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject projectile;
+    public float firerate;
+    public float range;
+
+    void Start()
+    {
+        StartCoroutine(do_shooting());
+    }
+
+    IEnumerator do_shooting()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(firerate);
+
+            GameObject next_target = count_enemies();
+            if(next_target != null)
+            {
+                Debug.Log("shoot");
+                Destroy(next_target);
+                MainButtons.instance.enemies.Remove(next_target);
+            }
+        }
+    }
+
+    GameObject count_enemies()
+    {
+        int cnt = 0;
+        float maxx = int.MinValue;
+        GameObject rez = null;
+
+        foreach(GameObject enemy in MainButtons.instance.enemies)
+        {
+            //Debug.Log(Vector2.Distance(enemy.transform.position, transform.position));
+            if (Vector2.Distance(enemy.transform.position, transform.position) < range && enemy.GetComponent<SplineAnimate>() != null)
+            {
+                cnt++;
+                Debug.Log("Ballon detected"); ///IT DETECTS
+                if(maxx - enemy.GetComponent<SplineAnimate>().NormalizedTime > 0)
+                {
+                    maxx = enemy.GetComponent<SplineAnimate>().NormalizedTime;
+                    rez = enemy;
+                }
+            }
+        }
+
+        return rez;
+    }
+}
