b99a53e72735d3aeb498cc45fa0f0feb2e807f66
Theman360 <158336022+tonealexandru236@users.noreply.github.com>
2025-08-07 12:01:35 -0400

    level 2 prototype

---
+++ C4GD-TD/Assets/Scripts/Enemy_change.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enemy_change : MonoBehaviour
+{
+    /*public Sprite space_e1;
+    public Sprite space_a1;
+    public Sprite space_a2;*/
+
+    public Sprite ocean_e1;
+    public Sprite ocean_a1;
+    public Sprite ocean_a2;
+
+    private SpriteRenderer sprite;
+    private Health hp;
+
+    void Start()
+    {
+        hp = GetComponent<Health>();
+
+        /*if (SceneManager.GetActiveScene().name == "Battle1")
+        {
+            sprite.sprite = space_e1;
+            hp.die_1 = space_a1;
+            if(space_a2 != null) hp.die_2 = space_a2;
+        }
+        else */if(SceneManager.GetActiveScene().name == "Battle2")
+        {
+            sprite.GetComponent<SpriteRenderer>().sprite = ocean_e1;
+            hp.die_1 = ocean_a1;
+            if (ocean_a2 != null) hp.die_2 = ocean_a2;
+        }
+    }
+}
